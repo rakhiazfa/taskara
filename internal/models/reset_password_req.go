3c6d0b502e3279fe74191376090d4c8d629e6b58
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2024-12-30 19:27:18 +0700

    feat(auth): adjust password field in reset password dto and sign up dto.

--- internal/models/reset_password_req.go
+++ internal/models/reset_password_req.go
@@ -3,5 +3,5 @@ package models
 type ResetPasswordReq struct {
 	Token                string `json:"token" validate:"required,max=100"`
 	Password             string `json:"password" validate:"required,min=8,max=100"`
-	PasswordConfirmation string `json:"passwordConfirmation" validate:"required,min=8,max=100,eqfield=Password"`
+	PasswordConfirmation string `json:"passwordConfirmation" validate:"required,eqfield=Password"`
 }
--- internal/models/sign_up_req.go
+++ internal/models/sign_up_req.go
@@ -1,7 +1,8 @@
 package models
 
 type SignUpReq struct {
-	Name     string `json:"name" validate:"required,max=100"`
-	Email    string `json:"email" validate:"required,email,max=100"`
-	Password string `json:"password" validate:"required,min=8,max=100"`
+	Name                 string `json:"name" validate:"required,max=100"`
+	Email                string `json:"email" validate:"required,email,max=100"`
+	Password             string `json:"password" validate:"required,min=8,max=100"`
+	PasswordConfirmation string `json:"passwordConfirmation" validate:"required,eqfield=Password"`
 }
